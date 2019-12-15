94ddf0dc60ad699de4e5e37e6839e2743f6720ac
Raghu Betina <raghu@firstdraft.com>
2019-12-15 20:03:58 +0000

    Validate presence of users.username

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
 
   # Validations
 
+  validates :username, :presence => true
+
   validates :username, :length => { :minimum => 4, :maximum => 20 }
 
   # Include default devise modules. Others available are:
