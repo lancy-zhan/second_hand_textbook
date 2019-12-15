ed4c906a92dd358f802a03badd0a49cfac520342
Raghu Betina <raghu@firstdraft.com>
2019-12-15 20:04:26 +0000

    Whitelist columns in Listing for ActiveAdmin

--- app/admin/listings.rb
+++ app/admin/listings.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Listing do
+
+ permit_params :transaction_id, :isbn, :course, :photo_url, :price, :description, :creator_id, :quarter, :school_year, :tittle
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
