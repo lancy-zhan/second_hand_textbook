d2e3b63ae2a13d11eb63dbd8f6baf7e2954fc03d
Raghu Betina <raghu@firstdraft.com>
2019-12-15 20:03:58 +0000

    Validate presence of listings.creator_id

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -5,6 +5,8 @@ class Listing < ApplicationRecord
 
   # Validations
 
+  validates :creator_id, :presence => true
+
   validates :isbn, :length => { :minimum => 10, :maximum => 13, :allow_blank => true }
 
   validates :tittle, :presence => true
